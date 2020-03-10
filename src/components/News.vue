9c7a988cb9f227fffc55909c3d8ce96d5e727b1c
asiahub-10 <asiarahman104@gmail.com>
2020-03-10 20:01:12 +0600

    project updated

--- src/components/News.vue
+++ src/components/News.vue
@@ -7,10 +7,22 @@
         </div>
       </div>
     </div>
-    <div class="container-fluid">
+    <!--<div class="container-fluid">-->
+      <!--<div class="row">-->
+        <!--<div class="col-sm-12 px-0">-->
+          <!--<iframe src="//lightwidget.com/widgets/51fbd82f45dc51f5ae83ecfdf668b46a.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>-->
+        <!--</div>-->
+      <!--</div>-->
+    <!--</div>-->
+    <div class="container-fluid bg-info">
       <div class="row">
-        <div class="col-sm-12 px-0">
-          <iframe src="//lightwidget.com/widgets/51fbd82f45dc51f5ae83ecfdf668b46a.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+        <div class="col-sm-12 pt-3">
+          <!-- SnapWidget -->
+          <!--<iframe src="https://snapwidget.com/embed/800114" class="snapwidget-widget w-100" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden;  width:960px; height:160px"></iframe>-->
+          <!-- SnapWidget -->
+          <!--<iframe src="https://snapwidget.com/embed/800114" class="snapwidget-widget w-100" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden;  width:960px; height:160px"></iframe>-->
+
+          <div class="elfsight-app-a3740ad6-fb60-40f4-a465-de9b4a4ca2df"></div>
         </div>
       </div>
     </div>
